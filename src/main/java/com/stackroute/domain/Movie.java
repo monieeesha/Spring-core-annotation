0e0426dfd979a3d432795b00d273b75b3aa917fe
monieeesha <monikadri13@gmail.com>
2019-05-27 14:33:25 +0530

    Task4 added

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,10 +1,19 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+
+//movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware
+{
 
     @Autowired
     private Actor actor;
@@ -44,5 +53,23 @@ public class Movie {
                 "actor=" + actor +
                 ", mname='" + mname + '\'' +
                 '}';
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(beanFactory.getBean("movie1"));
+    }
+
+    @Override
+    public void setBeanName(String s) {
+
+        System.out.println(s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException
+    {
+        System.out.println(applicationContext.getBean("movie1"));
     }
 }
